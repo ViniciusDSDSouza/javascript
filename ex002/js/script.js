e34e8c12fe486daf60a7eebe969bc69fdfb60107
Vinicius de Souza <viniciusdossantos547@gmail.com>
2023-02-28 15:37:16 -0300

    Criando ex002 e ex003

---
+++ ex002/js/script.js
@@ -0,0 +1,2 @@
+var nome = prompt('Qual o seu nome?')
+alert('É um prazer te conhecer, ' + nome + '!')
---
+++ ex003/js/script.js
@@ -0,0 +1,4 @@
+var n1 = Number(prompt('Digite um número:'))
+var n2 = Number(prompt('Digite outro número:'))
+s = n1 + n2
+alert('A soma de ' + n1 + ' + ' + n2 + ' = ' + s)
