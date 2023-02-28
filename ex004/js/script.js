0a73d7e929d85f5f3c9b8e8e124c6c0ee15db2d7
Vinicius de Souza <viniciusdossantos547@gmail.com>
2023-02-28 16:30:27 -0300

    ex004
    organizando os documentos html dos exercícios anteriores

---
+++ ex004/js/script.js
@@ -0,0 +1,7 @@
+var nome = prompt('Qual o seu nome?')
+
+document.write(`Olá <strong> ${nome}</strong>! seu nome possui ${nome.length} letras.<br>`)
+
+document.write(`Seu nome todo MAIÚSCULO fica <strong>${nome.toUpperCase()}</strong><br>`)
+
+document.write(`Seu nome todo em minúsculo fica <strong> ${nome.toLowerCase()}</strong><br>`)
